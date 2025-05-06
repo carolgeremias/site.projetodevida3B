ca29846a11fa97cb522e9dce49525918f15ddd3a
carolgeremias <146000560+carolgeremias@users.noreply.github.com>
2025-05-06 11:30:34 -0300

    .

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const botoes = document.querySelectorAll (".botoao");
 
 for (let i = 0; i <botoes.length; i++){
     botoes [i].onclick = function (){
-    for (let j = 0; j ,botoes.length; j++{
+    for (let j = 0; j < botoes.length; j++{
         botoes[j].classList.remove("ativo");
     }
 
