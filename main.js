9fb02ff76acabacd9cdfd977d54b351fe84f86c7
carolgeremias <146000560+carolgeremias@users.noreply.github.com>
2025-05-06 11:29:05 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes = document.querySelectorAll (".botoao"):
+const botoes = document.querySelectorAll (".botoao");
 
 for (let i = 0; i <botoes.length; i++){
     botoes [i].onclick = function (){
